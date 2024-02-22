bryanperris/ell

commit f7b7c24e3d2e0c14b99e0c4c7f2e3dba40684c20

pem: make endp setting smarter
Always doing eol + 1 could result in endp pointing past the bounds
of the PEM if the PEM does not have a final newline. Now we check
that we aren't going past the bounds of the buffer before setting
endp, and if we are just set endp to eol.

ell/pem.c
@@ -164,8 +164,12 @@ const char *pem_next(const void *buf, size_t buf_len, char **type_label,
 			if (base64_len)
 				*base64_len = buf_ptr - base64_data;
 
-			if (endp)
-				*endp = eol + 1;
+			if (endp) {
+				if (eol == buf + buf_len)
+					*endp = eol;
+				else
+					*endp = eol + 1;
+			}
 
 			return base64_data;
 		}

greearb/ell-ct

commit 7ee32349810482a1906303af23250890f0bb946a

pem: fix typo in a conditional in pem_next()
This conditional is supposed to check if there is an extra (whitespace)
character after the ending boundary of the PEM. However, it does not
accomplish that goal as it is written because of a typo.

Fixes: f7b7c24e3d2e ("pem: make endp setting smarter")

ell/pem.c
@@ -148,7 +148,7 @@ const char *pem_next(const void *buf, size_t buf_len, char **type_label,
 				*base64_len = buf_ptr - base64_data;
 
 			if (endp) {
-				if (eol == buf + buf_len)
+				if (eol == buf_ptr + buf_len)
 					*endp = eol;
 				else
 					*endp = eol + 1;