greearb/ell-ct

commit d8a703340f1ea32a65af44fb1407acf55523628c

utf8: Add l_ascii_strdown

ell/utf8.c
@@ -1,6 +1,7 @@
 /*
  * Embedded Linux library
  * Copyright (C) 2011-2014  Intel Corporation
+ * Copyright (C) 2024  Cruise, LLC
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
@@ -41,6 +42,38 @@ LIB_EXPORT unsigned char l_ascii_table[256] = {
 	[0x7F]		= L_ASCII_CNTRL,
 	[0x80 ... 0xFF] = 0,
 };
+
+/**
+ * l_ascii_strdown
+ * @str: a pointer to an ASCII string
+ * @len: maximum bytes to process or negative if string is null terminated
+ *
+ * Returns: Newly allocated string with all upper case characters converted
+ * to lower case.
+ **/
+LIB_EXPORT char *l_ascii_strdown(const char *str, ssize_t len)
+{
+	size_t slen;
+	size_t i;
+	char *ret;
+
+	if (!str)
+		return NULL;
+
+	if (len < 0)
+		slen = strlen(str);
+	else
+		slen = minsize(strlen(str), (size_t) len);
+
+	ret = l_malloc(slen + 1);
+
+	for (i = 0; i < slen; i++)
+		ret[i] = l_ascii_tolower(str[i]);
+
+	ret[i] = '\0';
+
+	return ret;
+}
 
 static inline bool __attribute__ ((always_inline))
 			valid_unicode(wchar_t c)

ell/ell.sym
@@ -49,6 +49,7 @@ global:
 	l_strv_eq;
 	/* utf8 */
 	l_ascii_table;
+	l_ascii_strdown;
 	l_utf8_get_codepoint;
 	l_utf8_validate;
 	l_utf8_strlen;

ell/utf8.h
@@ -103,6 +103,8 @@ static inline __attribute__ ((always_inline)) char l_ascii_tolower(char c)
 	return c + 32;
 }
 
+char *l_ascii_strdown(const char *str, ssize_t len);
+
 bool l_utf8_validate(const char *src, size_t len, const char **end);
 size_t l_utf8_strlen(const char *str);
 

commit 57fcbf80bff6fdbf10b21bd4d3f3e282f14599b9

utf8: Do not use strlen when size is provided
The intent of passing a positive len was to limit the number of bytes
accessed in cases where input is not NULL terminated.

Fixes: d8a703340f1e ("utf8: Add l_ascii_strdown")
Fixes: 86c28534cd47 ("utf8: Add l_ascii_strup")

ell/utf8.c
@@ -63,11 +63,11 @@ LIB_EXPORT char *l_ascii_strdown(const char *str, ssize_t len)
 	if (len < 0)
 		slen = strlen(str);
 	else
-		slen = minsize(strlen(str), (size_t) len);
+		slen = len;
 
 	ret = l_malloc(slen + 1);
 
-	for (i = 0; i < slen; i++)
+	for (i = 0; i < slen && str[i]; i++)
 		ret[i] = l_ascii_tolower(str[i]);
 
 	ret[i] = '\0';
@@ -95,11 +95,11 @@ LIB_EXPORT char *l_ascii_strup(const char *str, ssize_t len)
 	if (len < 0)
 		slen = strlen(str);
 	else
-		slen = minsize(strlen(str), (size_t) len);
+		slen = len;
 
 	ret = l_malloc(slen + 1);
 
-	for (i = 0; i < slen; i++)
+	for (i = 0; i < slen && str[i]; i++)
 		ret[i] = l_ascii_toupper(str[i]);
 
 	ret[i] = '\0';