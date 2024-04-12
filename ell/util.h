f0320e5492bcd16200b49b3451d344e0c7093329
Denis Kenzior <denkenz@gmail.com>
2024-04-12 14:58:10 -0500

    util: Add L_BITS_{SET|CLEAR}
    These macros can set or clear an arbitrary number of bits in a single
    call, as follows:
    
    	uint64_t bitmap = 0;
    
    	L_BITS_CLEAR(&bitmap, 0, 1, 5, 15, 32, 63);

--- ell/util.h
+++ ell/util.h
@@ -396,6 +396,20 @@ inline __attribute__((always_inline)) void _l_close_cleanup(void *p)
 	(*_offset & _L_BIT_TO_MASK(_offset, _nr)) != 0;			\
 })
 
+#define L_BITS_SET(bits, ...) __extension__ ({				\
+	const unsigned int __elems[] = {__VA_ARGS__};			\
+	size_t __i;							\
+	for (__i = 0; __i < L_ARRAY_SIZE(__elems); __i++)		\
+		L_BIT_SET(bits, __elems[__i]);				\
+})
+
+#define L_BITS_CLEAR(bits, ...) __extension__ ({			\
+	const unsigned int __elems[] = {__VA_ARGS__};			\
+	size_t __i;							\
+	for (__i = 0; __i < L_ARRAY_SIZE(__elems); __i++)		\
+		L_BIT_CLEAR(bits, __elems[__i]);			\
+})
+
 /*
  * Taken from https://github.com/chmike/cst_time_memcmp, adding a volatile to
  * ensure the compiler does not try to optimize the constant time behavior.
